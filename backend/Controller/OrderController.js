f346b441da64f4a7eb350d57fdce1b78e6fdbfe7
Ranga0206 <ranga02062000@gmail.com>
2026-01-19 22:07:23 +0530

    OrderCreation and OrderUpdate and Order Delete

--- backend/Controller/OrderController.js
+++ backend/Controller/OrderController.js
@@ -1,4 +1,8 @@
+import HandleError from "../helper/handleError.js";
 import Order from "../models/OrderModel.js";
+import Product from "../models/ProductModel.js";
+
+//Create new Order
 export const createNewOrder = async (req, res, next) => {
   const {
     shippingAddress,
@@ -25,4 +29,101 @@ export const createNewOrder = async (req, res, next) => {
     success: true,
     order,
   });
+};
+
+//Get Single Order
+export const getOrderDetails = async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email",
+  );
+  if (!order) {
+    return next(new HandleError("Order not found", 400));
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+};
+
+//Get all order details
+export const getAllOrders = async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+  if (!orders) {
+    return next(new HandleError("Orders not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+};
+
+//Get All Orders by Admin
+export const getAllOrdersByAdmin = async (req, res, next) => {
+  const orders = await Order.find();
+  if (!orders) {
+    return next(new HandleError("Orders not found", 404));
+  }
+  let totalAmount = 0;
+  orders.forEach((order) => (totalAmount += order.totalPrice));
+  res.status(200).json({
+    success: true,
+    orders,
+    totalAmount,
+  });
+};
+
+//Admin Delete Orders
+export const deleteOrder = async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new HandleError("Order not found", 404));
+  }
+  if (order.orderStatus !== "Delivered") {
+    return next(
+      new HandleError(
+        "This order is under processing and cannot be deleted",
+        404,
+      ),
+    );
+  }
+  await Order.deleteOne({ _id: req.params.id });
+  res.status(200).json({
+    success: true,
+    message: "Order Deleted successfully",
+  });
+};
+
+//Admin Order Update
+export const updateOrderStatus = async (req, res, next) => {
+  const id = req.params.id;
+  const order = await Order.findById(id);
+  if (!order) {
+    return next(new HandleError("Order not found", 404));
+  }
+  if (order.orderStatus == "Delivered") {
+    return next(new HandleError("This order is already been delivered", 404));
+  }
+  //Update stock
+  await Promise.all(
+    order.orderItems.map((item) => updateQuantity(item.product, item.quantity)),
+  );
+  order.orderStatus = req.body.status;
+  if (order.orderStatus === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+    order,
+  });
+};
+
+const updateQuantity = async (id, quantity) => {
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new Error("Product not Found");
+  }
+  product.stock -= quantity;
+  await product.save({ validateBeforeSave: false });
 };
--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { verfiyUser } from "../helper/UserAuth.js";
-import { createNewOrder } from "../Controller/OrderController.js";
+import { roleBasedAccess, verfiyUser } from "../helper/UserAuth.js";
+import {
+  createNewOrder,
+  getAllOrders,
+  getOrderDetails,
+  getAllOrdersByAdmin,
+  deleteOrder,
+} from "../Controller/OrderController.js";
 const router = express.Router();
 
 router.route("/new/order").post(verfiyUser, createNewOrder);
+router.route("/order/:id").get(verfiyUser, getOrderDetails);
+router.route("/orders/user").get(verfiyUser, getAllOrders);
 
+//admin
+router
+  .route("/admin/orders")
+  .get(verfiyUser, roleBasedAccess("admin"), getAllOrdersByAdmin);
+
+router
+  .route("/admin/order/:id")
+  .delete(verfiyUser, roleBasedAccess("admin"), deleteOrder);
 export default router;
